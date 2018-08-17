cd2f750fa626799bf046b7b76f39ff0b3e83b01f
Vijayaragavi <41079002+VIJAYARAHAVI@users.noreply.github.com>
2018-08-17 00:25:47 -0700

    Update positive.java

--- positive.java
+++ positive.java
@@ -1,2 +1,20 @@
-# guvi
-activity
+import java.util.Scanner;
+
+public class positive {
+public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+	int n=s.nextInt();
+	if(n>0)
+	{
+		System.out.println("Positive");
+	}
+	else if(n<0)
+	{
+		System.out.println("Negative");
+	}
+	else
+	{
+		System.out.println("Zero");
+	}
+}
+}
