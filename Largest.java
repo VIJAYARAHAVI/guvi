70dcc2b8acf3412dda59626a602eadbbd4041447
Vijayaragavi <41079002+VIJAYARAHAVI@users.noreply.github.com>
2018-08-17 02:38:34 -0700

    Update Largest.java

--- Largest.java
+++ Largest.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Largest {
 public static void main(String[] args) {
 	Scanner s=new Scanner(System.in);
-	int a=s.nextInt(),b=s.nextInt(),c=s.nextInt(),temp=0;
+	int a=s.nextInt(),b=s.nextInt(),c=s.nextInt(),temp=c;
 	if((a>b) && (a>c))
 	{
 		temp=a;
