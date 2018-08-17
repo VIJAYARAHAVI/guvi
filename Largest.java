646ffbb7624882e0736a9d76261d2754b76169e4
Vijayaragavi <41079002+VIJAYARAHAVI@users.noreply.github.com>
2018-08-17 02:35:41 -0700

    Create Largest.java

---
+++ Largest.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Largest {
+public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+	int a=s.nextInt(),b=s.nextInt(),c=s.nextInt(),temp=0;
+	if((a>b) && (a>c))
+	{
+		temp=a;
+	}
+	else if(b>c)
+	{
+		temp=b;
+	}
+	else
+	{
+		temp=c;
+	}
+	System.out.println(temp);
+	
+	}
+}
